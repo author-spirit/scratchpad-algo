2090b45c53a3deaa505c5f0da95afcc4529b1139
author-spirit <harishkshatriya.msr@gmail.com>
2025-06-22 09:41:48 +0530

    Finalized the Reverse Stack

--- Recursion/ReverseStack.py
+++ Recursion/ReverseStack.py
@@ -1,13 +1,24 @@
-stack = [5,4,3,2,1]
-aux = []
-def do_reverse(stack):
+stack = [1,2,3,4,5]
+
+def reverse(stack):
     if not stack:
+        return stack
+
+    val = stack.pop()
+    reverse(stack)
+    print("Printing")
+    insert(stack, val)
+
+def insert(stack, val):
+    print("Insert", stack, val)
+    if not stack:
+        stack.append(val)
         return
     
-    num = stack.pop()
-    do_reverse(stack)
-    stack.append(num)
+    stack_val = stack.pop()
+    insert(stack,val)
+    stack.append(stack_val)
 
 print("Before", stack)
-do_reverse(stack)
+reverse(stack)
 print("After", stack)
