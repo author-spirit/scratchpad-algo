c04df95768487f529c8225c37ac0b021b86104fe
author-spirit <harishkshatriya.msr@gmail.com>
2025-11-04 22:02:15 +0530

    Graph: CourseSchedule I v2

--- Graphs/CourseSchedule.py
+++ Graphs/CourseSchedule.py
@@ -2,43 +2,36 @@
 Ref: https://leetcode.com/problems/course-schedule/description/
 """
 
-# Cyclic Graph
-prereq = [[0,1], [0,2], [1,3], [1,4], [3,4]]
-prereq=[[1,0],[0,1]]
-n=2
-adj = {i:[] for i in range(n)}
+def can_finish(numCourses: int, prerequisites):
+    adjList = {n: [] for n in range(numCourses)}
+    for cur, pre in prerequisites:
+        adjList[cur].append(pre)
 
-# convert to adjacent list
-for cur,pre in prereq:
-    adj[cur].append(pre)
-
-
-visited = set()
-def is_cyclic(node):
-    if node in visited:
-        return True
-
-    if adj[node] == []:
-        return False
-
-    visited.add(node)
-
-    # loop over the node's adjacent list
-    for _node in adj[node]:
-        if is_cyclic(_node):
+    visited = [None]*numCourses
+    def is_cyclic(n):
+        if visited[n] == 1:
             return True
 
-    # Deleting current node from visited set as other node treat it like cyclic
-    # Keep the current node adjlist empty to prevent multiple DFS scan on same node
-    visited.remove(node)
-    adj[node] = []
+        if visited[n] == 2:
+            return False
 
-    return False
+        visited[n] = 1
+        for i in adjList[n]:
+            if is_cyclic(i):
+                return True
 
-can_take = True
-for i in range(n):
-    if is_cyclic(i):
-        can_take = False
-        break
+        visited[n] = 2
+        return False
 
-print("Can Take Course", can_take)
+
+    for n in range(numCourses):
+        if is_cyclic(n):
+            return False
+
+    return True
+
+prereq = [[0,1], [0,2], [1,3], [1,4], [3,4]]
+# prereq=[[1,0],[0,1]]
+n=5
+
+print("Can finish the course? ", can_finish(n,prereq))
